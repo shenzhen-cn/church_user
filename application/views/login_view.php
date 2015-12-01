6eb76c518d3ea882a71204f83ccf33f7c5db555b
shenzheniapps <zhen@iappsasia.com>
2015-12-01 20:43:18 +0800

    pp

--- application/views/login_view.php
+++ application/views/login_view.php
@@ -2,11 +2,10 @@
 <html lang="zh-CN">
 <head>
 <title>登录</title>
-<?php $this->load->view('tq_head'); ?>
+
 </head>
   <body class="hold-transition login-page">
-    <div class="login-box">
-      <div class="login-box-body">
+    
         <p class="login-box-msg">登录</p>
         <form action="<?php echo site_url('login/testphp'); ?>" method="get">
 
@@ -21,13 +20,6 @@
         <a type="button" href="<?php echo site_url('forgetpassword'); ?>">忘记密码？</a><br>
 
         </form>
-      </div><!-- /.login-box-body -->
-    </div><!-- /.login-box -->
-
-    <!-- jQuery 2.1.4 -->
-    <script src="<?php echo base_url(); ?>public/plugins/js/jquery-2.1.4.min.js"></script>
-    <!-- Bootstrap 3.3.5 -->
-    <script src="<?php echo base_url(); ?>public/js/bootstrap.min.js"></script>
 
   </body>
 </html>
