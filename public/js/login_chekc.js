8f0acd47530d8c8d992659629ae822b5b6a3c58e
shenzheniapps <zhen@iappsasia.com>
2015-12-01 22:46:14 +0800

    pp

---
+++ public/js/login_chekc.js
@@ -0,0 +1,37 @@
+
+function  changing() {
+	document.getElementById('checkpic').src="public/images/checkcode.php?"+Math.random();
+}
+
+//登录表单验证
+(function($) {
+	$().ready(function() {
+	 $("#check_user_login").validate({
+	  rules: {
+	   user_name: "required",
+	   password: {
+	    required: true,
+	   },
+	   checkcode: {
+	    required: true,
+	   }
+	  },
+	  messages: {
+	   user_name: "*请输入用户名",
+	   password: {
+	    required: "*请输入密码",
+	   },
+	   checkcode: {
+	    required: "*请输入确认验证码",
+	   }
+	  }
+	    });
+	});
+
+})(jQuery);
+
+
+
+
+
+
