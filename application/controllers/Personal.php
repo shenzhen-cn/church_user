843e3ade2210b4bc8e40eeb3b82c75f24e9742a1
shenzheniapps <zhen@iappsasia.com>
2015-12-01 14:32:56 +0800

    ppp

--- application/controllers/Personal.php
+++ application/controllers/Personal.php
@@ -61,8 +61,8 @@ class Personal extends MY_Controller {
 				$params['userHeadSrc'] = $userHeadSrc; 
 			}else {
 				$fileInfo = $_FILES['uploadphoto'];
-				$uploadPath = "/var/www/html/church/church_user/public/uploads/userHeadsrc";
-				$msg_return = uploadFiles( $fileInfo,$uploadPath);
+				$uploadPath = "public/uploads/userHeadsrc";
+				$msg_return = uploadFiles($fileInfo,$uploadPath);
 				var_dump($msg_return);exit;
 				if (isset($msg_return['msg']) ) {
 					$this->session->set_flashdata('error', $msg_return['msg']);
