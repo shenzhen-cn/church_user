29bc6d5084aedde8b4a58638ea87405b5eccd640
shenzheniapps <zhen@iappsasia.com>
2015-12-01 20:50:58 +0800

    pp

--- application/views/login_view.php
+++ application/views/login_view.php
@@ -9,6 +9,7 @@
     First name: <input type="text" name="fname" required="required" /><br />
     Last name: <input type="text" name="lname"  required="required"/><br />
     E-mail: <input type="text" name="email" autocomplete="off"  required="required" /><br />
+     Name: <input type="text" name="usr_name" required="required" />
     <input type="submit" />
     </form>
 
