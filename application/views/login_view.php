93e61db4ec07e267fa1c0001e7a25cb86f1a6edb
shenzheniapps <zhen@iappsasia.com>
2015-12-01 21:28:08 +0800

    pp

--- application/views/login_view.php
+++ application/views/login_view.php
@@ -2,17 +2,56 @@
 <html lang="zh-CN">
 <head>
 <title>登录</title>
-
+<?php $this->load->view('tq_head'); ?>
 </head>
-  <body >
-    <form action="<?php echo site_url('login/testphp'); ?>" method="get" autocomplete="on">
-    First name: <input type="text" name="fname" required="required" /><br />
-    Last name: <input type="text" name="lname"  required="required"/><br />
-    E-mail: <input type="text" name="email" required /><br />
-     Name: <input type="text" name="usr_name" required /><br />
-    <input type="submit" />
-    </form>
+  <body class="hold-transition login-page">
+    <div class="login-box">
+    <?php $this->load->view('tq_alerts'); ?>
+      <div class="login-logo">
+        <b>使命</b>青年团契
+      </div><!-- /.login-logo -->
+      <div class="login-box-body">
+        <p class="login-box-msg">登录</p>
+        <form action="<?php echo site_url('login'); ?>" method="post">
+          <div class="form-group has-feedback">
+            <input type="email" class="form-control"  id="user_name" name="user_name" placeholder="邮箱账号:" required="required" />
+            <span class="fa fa-envelope-o  form-control-feedback"></span>
+          </div>
+          <div class="form-group has-feedback">
+            <input type="password" class="form-control" id="password" name="password" placeholder="密码:" required="required"/>
+            <span class="fa fa-key form-control-feedback"></span>
+          </div>
+          <div class="row">
+            <div class="form-group col-xs-4">
+               <input type="text" class="form-control" id="checkcode" name="checkcode" placeholder="验证码:" required="required" AUTOCOMPLETE="off"/>
+            </div><!-- /.col -->
+            <div class="form-group col-xs-4">
+               <img id="checkpic" onclick="changing()" src='<?php echo base_url();?>public/images/checkcode.php'>
+            </div><!-- /.col -->
+            <div class="form-group col-xs-4">
+               <a href="javascript:;"  onclick="changing()" style="color:red;"><small style="width:80px;height:34px">看不清，点击刷新</small></a>
+            </div><!-- /.col -->
+          </div>
 
+        <br>
+        <button type="submit" class="btn btn-primary btn-block btn-flat">登录</button><br>
+        <a type="button" href="<?php echo site_url('forgetpassword'); ?>">忘记密码？</a><br>
+
+        </form>
+      </div><!-- /.login-box-body -->
+    </div><!-- /.login-box -->
+
+    <!-- jQuery 2.1.4 -->
+    <script src="<?php echo base_url(); ?>public/plugins/js/jquery-2.1.4.min.js"></script>
+    <!-- Bootstrap 3.3.5 -->
+    <script src="<?php echo base_url(); ?>public/js/bootstrap.min.js"></script>
+    <script type="text/javascript">
+      function  changing() {
+          document.getElementById('checkpic').src="../public/images/checkcode.php?"+Math.random();
+      }
+
+    </script>
+    
   </body>
 </html>
 
