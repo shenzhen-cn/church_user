3b12be2fad6241aad4262265c0fa1495f76e82c2
shenzheniapps <zhen@iappsasia.com>
2015-12-01 20:48:29 +0800

    pp

--- application/views/login_view.php
+++ application/views/login_view.php
@@ -4,22 +4,13 @@
 <title>登录</title>
 
 </head>
-  <body class="hold-transition login-page">
-    
-        <p class="login-box-msg">登录</p>
-        <form action="<?php echo site_url('login/testphp'); ?>" method="get">
-
-          <div class="form-group has-feedback">
-            <input type="password" class="form-control" id="password" name="password" placeholder="密码:" required="required"/>
-            <span class="fa fa-key form-control-feedback"></span>
-          </div>
-        
-
-        <br>
-        <button type="submit" class="btn btn-primary btn-block btn-flat">登录</button><br>
-        <a type="button" href="<?php echo site_url('forgetpassword'); ?>">忘记密码？</a><br>
-
-        </form>
+  <body >
+    <form action="<?php echo site_url('login/testphp'); ?>" method="get" autocomplete="on">
+    First name: <input type="text" name="fname" required="required" /><br />
+    Last name: <input type="text" name="lname"  required="required"/><br />
+    E-mail: <input type="text" name="email" autocomplete="off"  required="required" /><br />
+    <input type="submit" />
+    </form>
 
   </body>
 </html>
